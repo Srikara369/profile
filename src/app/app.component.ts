a963ef26768bf1cb58aaaa89566d1b7be486f85d
srikara369 <srikara.b@gmail.com>
2022-05-04 19:13:55 +0530

    Navbar and short summary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Resume';
+
+export class AppComponent implements OnInit {
+  title = 'Profile';
+  headings:any = ["About", "Education", "Skills"]
+  windowWidth: any = window.innerWidth
+
+  constructor(){
+    window.addEventListener('resize', (event) => {
+      this.windowWidth = window.innerWidth
+    })
+  }
+
+  ngOnInit() {
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
