25040c821e0ca0ad7adc80ba36fae7ef8495a45e
srikara369 <srikara.b@gmail.com>
2022-05-04 23:08:13 +0530

    ..

--- src/app/app-config.ts
+++ src/app/app-config.ts
@@ -1,5 +1,5 @@
 export var config = {
     "build": true,
     "devAssetPath": "../../assets",
-    "buildAssetPath": "https://github.com/srikara369/profile/tree/main/docs/assets"
+    "buildAssetPath": "assets"
 }
